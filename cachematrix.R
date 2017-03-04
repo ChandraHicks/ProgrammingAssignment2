e7f5b380a504354d1edb4a2e757125a8102c84af
ChandraHicks <chandrahicks@gmail.com>
2017-03-04 13:31:28 -0800

    final homework 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## The following 2 functions will cache the inverse of a matrix so that the
 ## computation doesn't need to happen more than once.
 
-## Write a short comment describing this function
+## This function stores information about a matrix, including functions to 
+## calculate the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -20,7 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function will solve for the inverse, but once it's done that, it will be cached
+## for future use - the input is an object from the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
