010f845d555fa1256136ec68fda9b45891141bbf
ChandraHicks <chandrahicks@gmail.com>
2017-03-04 13:26:59 -0800

    final homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following 2 functions will cache the inverse of a matrix so that the
+## computation doesn't need to happen more than once.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  
 }
 
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    m <- x$getsolve()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
+  
 }
